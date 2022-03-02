fb5a32c3f9c9e1bf7c88029267fa39d2aa497ae4
SauleStan <vogellietuva@gmail.com>
2022-03-02 15:49:49 +0200

    Fixed IsSolvable function

--- AStar.py
+++ AStar.py
@@ -1,15 +1,6 @@
 import heapq
 from Node import Node
-from PriorityEntry import PriorityEntry
 from Actions import Action
-
-initialState = [2, 3, 7, 1, 4, 8, 0, 6, 5]
-# initialState = [0, 1, 3, 4, 2, 6, 7, 5, 8]
-goalState = [1, 2, 3, 4, 5, 6, 7, 8, 0]
-depth = 0
-
-# Starting node
-initialNode = Node(initialState, None, depth, goalState)
 
 #  Function to recursively return the states from the start node to the supplied node
 def Solution(node):
@@ -20,14 +11,16 @@ def Solution(node):
 
 # Function to check whether puzzle is solvable
 # Inversion count on the state list should be even, then puzzle is solvable
-# Since the array index for the number is the same as index+1,
-# We can check the amount of inversions by taking away the index place from the number
 def IsSolvable(state):
     inversions = 0
-    for index, number in enumerate(state):
-        if(number==0):
+    for i in range(len(state)):
+        if(state[i]==0):
             continue
-        inversions += abs(number - (index + 1))
+        for j in range(i,len(state)):
+            if(state[j]==0):
+                continue
+            if(state[i]>state[j]):
+                inversions += 1
     
     # If inversions count is not even, puzzle is not solvable
     if (inversions % 2) != 0:
@@ -35,32 +28,39 @@ def IsSolvable(state):
     else:
         return True
 
-# Use heap as frontier list for priority queue behaviour
-frontier = []
-heapq.heappush(frontier, (initialNode.fval, initialNode.state, initialNode))
-
-# Put initial node into queue using fval of the node as priority 
-# frontier.put(PriorityEntry(initialNode.fval, initialNode))
-
-# Set of explored nodes
-explored = []
-
 if __name__ == "__main__":
-    actions = ["UP", "DOWN", "LEFT", "RIGHT"]
-    currentState = initialState
-    
-    while True:
-        solvable = IsSolvable(currentState)
-        if (not solvable):
-            print("Initial state is unsolvable.")
-            break
+    initialState = [2, 3, 7, 1, 4, 8, 0, 6, 5]
+    goalState = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+    depth = 0
 
+    # Starting node
+    initialNode = Node(initialState, None, depth, goalState)
+
+    # Use heap as frontier list for priority queue behaviour
+    frontier = []
+    heapq.heappush(frontier, (initialNode.fval, initialNode.state, initialNode))
+
+    # Set of explored nodes
+    explored = []
+
+    # Availbale actions to perform on the game state
+    actions = ["UP", "DOWN", "LEFT", "RIGHT"]
+    
+    # If initial state is unsolvable, stops the program
+    solvable = IsSolvable(initialState)
+    if (not solvable):
+        print("Initial state is unsolvable.")
+        quit()
+
+    while True:
+        # If frontier is empty, stops the program
         if len(frontier) == 0:
             break
         
+        # Gets the smallest cost node from the frontier
         currentNode = heapq.heappop(frontier)[2]
 
-        # If there are no more misplaced tiles, the state is returned
+        # If there are no more misplaced tiles, the solution is displayed
         if currentNode.IsGoal():
             Solution(currentNode)
             break
@@ -68,11 +68,11 @@ if __name__ == "__main__":
         # If node is not goal, add it to explored list
         explored.append(currentNode)
 
-        # Loop through actions
+        # Perform actions
         for action in actions:
             # Get new state from action
             currentState = Action(action, currentNode.state)
-            # If action doesn't return a node, continue to next action
+            # When action is not possible, returns null, continue to next action
             if currentState == None:
                 continue
             # Create child node with the new state
@@ -80,6 +80,3 @@ if __name__ == "__main__":
             # If child node doesn't exist in explored set or frontier, add it to the frontier
             if not(any(childNode.state == item.state for item in explored) or any(childNode.state in item for item in frontier)):
                 heapq.heappush(frontier, (childNode.fval, childNode.state, childNode))
-        
-        
-            
