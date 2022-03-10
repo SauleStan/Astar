360247c4eebeb4332e337894e764e69412d787ec
SauleStan <vogellietuva@gmail.com>
2022-03-10 16:57:54 +0200

    Added check for no args

--- Parser.py
+++ Parser.py
@@ -5,4 +5,7 @@ def ParseCmd():
                         help='List of integers for state of 8-puzzle, e.g. 1 2 3 4 5 6 7 8 0\nAdd the spaces between!')
 
     args = parser.parse_args()._get_kwargs()
+    if(args[0][1] is None):
+        print("Invalid argument for initial state, use py Astar.py -h for usage info")
+        quit()
     return args[0][1]
